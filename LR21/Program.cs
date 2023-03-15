8de58a923e4b5e9b46b5246fb750c0565ecf32ee
tophi <tophi@DESKTOP-NIMVBMP>
2023-03-15 14:42:27 +0300

    Добавьте файлы проекта.

---
+++ LR21/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Выберите операцию (+, -, *, /):");
+        char op = Console.ReadLine()[0];
+
+        double result = 0;
+        switch (op)
+        {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0)
+                {
+                    Console.WriteLine("Деление на ноль!");
+                    return;
+                }
+                result = a / b;
+                break;
+            default:
+                Console.WriteLine("Неверная операция!");
+                return;
+        }
+
+        Console.WriteLine("Результат: {0}", result);
+    }
+}
